7b6a3609de88e4ac602d5dc9ef14dcc63793d061
Obaid Qatan <ogaten27@gmail.com>
2022-12-11 05:52:31 +0530

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { camelCase, startCase } from "lodash";
 import type { NextPage } from "next";
 import useTranslation from "next-translate/useTranslation";
+import Link from "next/link";
 import RegularButton from "../src/components/common/Buttons/Regular";
 import Card from "../src/components/common/Card";
 import Logo from "../src/components/Layout/Logo";
@@ -12,9 +13,9 @@ const Home: NextPage = () => {
     <Card className="rounded-none border-none shadow-none h-screen flex flex-col justify-center items-center">
       <Logo />
       <RegularButton>
-        <a href="/customers">
-          {startCase(tCommon(camelCase("view all custoners")))}
-        </a>
+        <Link href="/customers">
+          <a>{startCase(tCommon(camelCase("view all custoners")))}</a>
+        </Link>
       </RegularButton>
     </Card>
   );
